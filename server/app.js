5118cea0f36c9ab600f53b0450640d5a2421b97d
Aries Cs <aries0d0f@gmail.com>
2018-02-13 13:40:42 +0800

    Add image upload API.

--- server/app.js
+++ server/app.js
@@ -2,10 +2,21 @@ const Koa               = require('koa')
 const KoaJson           = require('koa-json')
 const bodyParser        = require('koa-bodyparser')
 const cors              = require('@koa/cors')
+const path              = require('path')
+const uploader          = require('koa2-file-upload')
 
 const route             = require('./route')
 const config            = require('../config.json')
 const app               = new Koa()
+
+const options = {
+  url: '/upload',
+  storeDir: '',
+  provider: 'local',
+  mimetypes: ['image/png', 'image/jpg', 'image/jpeg'],
+  folder: 'public/images',
+  urlPath: 'images'
+}
 
 app.use(bodyParser())
 app.use(KoaJson())
@@ -19,4 +30,6 @@ app.use(cors({
   allowMethods: ['GET', 'POST', 'DELETE', 'PUT']
 }))
 app.use(route.routes())
+app.use(uploader(options))
+app.use(require('koa-static')(path.join(__dirname, '/../public/')))
 app.listen(config.port)
--- server/model/index.js
+++ server/model/index.js
@@ -1,8 +1,8 @@
-const news           = require('./news')
-const post           = require('./post')
-const user           = require('./user')
-const test           = require('./test')
-const docs           = require('./docs')
-const creation       = require('./creation')
+const news            = require('./news')
+const post            = require('./post')
+const user            = require('./user')
+const test            = require('./test')
+const docs            = require('./docs')
+const creation        = require('./creation')
 
 module.exports = { news, post, user, test, docs, creation }
