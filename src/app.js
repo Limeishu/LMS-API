c83ec7b2d3a4b5489bbb5b7a00662df9703fba9c
Aries Cs <aries0d0f@gmail.com>
2018-07-28 05:00:07 +0800

    Remove old file API and create a new one (empty)

--- src/app.js
+++ src/app.js
@@ -3,22 +3,12 @@ import KoaJson          from 'koa-json'
 import bodyParser       from 'koa-bodyparser'
 import cors             from '@koa/cors'
 import path             from 'path'
-import uploader         from 'koa2-file-upload'
 import koaStatic        from 'koa-static'
 
 import route            from './route'
 import config           from '../config.json'
 
 const app               = new Koa()
-
-const options = {
-  url: '/upload',
-  storeDir: '',
-  provider: 'local',
-  mimetypes: ['image/png', 'image/jpg', 'image/jpeg'],
-  folder: 'public/images',
-  urlPath: 'images'
-}
 
 app.use(bodyParser())
 app.use(KoaJson())
@@ -32,6 +22,5 @@ app.use(cors({
     allowMethods: 'GET'
   }))
 app.use(route.routes())
-app.use(uploader(options))
 app.use(koaStatic(path.join(__dirname, '/../public/')))
 app.listen(config.port)
--- src/model/index.js
+++ src/model/index.js
@@ -3,7 +3,8 @@ import post             from './post'
 import user             from './user'
 import test             from './test'
 import docs             from './docs'
+import upload           from './upload'
 import creation         from './creation'
 import firewallLog      from './firewallLog'
 
-export default { news, post, user, test, docs, creation, firewallLog }
+export default { news, post, user, test, docs, upload, creation, firewallLog }
